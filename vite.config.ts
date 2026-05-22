2b212dccd55eebcb14b61d3589a9eb80381ee9fb
Jayden Maans <maansjayden56@gmail.com>
2026-05-22 10:21:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './', // <--- ADD THIS LINE
+    base: './', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
